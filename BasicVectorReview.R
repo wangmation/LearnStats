f53493bff420c8de41262469fa2adb6099dc151e
YX Wang <wangmation@gmail.com>
2021-08-17 14:07:51 +0900

    Add files via upload

---
+++ BasicVectorReview.R
@@ -0,0 +1,22 @@
+# Create a vector
+v1=c(1:100)
+v2=c(77,60,91,73,85,82,35,100,66,75)
+
+# Square root of v1
+print(v1^1/3)
+print(sqrt(v1))
+
+# Ascending & Descending listing in v2
+print(sort(v2)) # Ascending
+print(rev(sort(v2))) # Reverse to Descend
+
+# Statistical formula in v2
+print(mean(v2)) # Average
+print(sd(v2)) # Standard Difference
+print(min(v2)) # Minimum
+print(max(v2)) # Maximum
+print(median(v2)) # Median
+
+# Create a sequential vector
+v3=seq(0,1,length.out=100)
+print(v3)
