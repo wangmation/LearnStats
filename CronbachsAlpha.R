f13c2cd0c0c600b505952c0d694f1aedf33ca966
Y.X Wang <yxdotwang@YX-Wangs-Desktop-Mac-mini.local>
2025-06-22 15:42:31 +0800

    Cronbachs-Alpha and KMO examples from Internet.

---
+++ CronbachsAlpha.R
@@ -0,0 +1,23 @@
+# Load Package
+library(ltm)
+
+# Using sample data here
+data <- data.frame(Q1=c(1, 2, 2, 3, 2, 2, 3, 3, 2, 3),
+                   Q2=c(1, 1, 1, 2, 3, 3, 2, 3, 3, 3),
+                   Q3=c(1, 1, 2, 1, 2, 3, 3, 3, 2, 3))
+
+# Calculate
+print(cronbach.alpha(data,CI=TRUE))
+
+# KMO Validation
+
+# Load Package
+library(psych)
+
+# Calculate
+KMO_result=KMO(data)
+print(KMO_result)
+
+# Bartlett Calculate
+bartlett_result=cortest.bartlett(data)
+print(bartelett_result)
